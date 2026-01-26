PCA vs RAW data KMEANS and GMM training + pipeline on KNN and RFC to fix leakage

src/StarClassifier.py
@@ -2,25 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.calibration import LinearSVC
-from sklearn.discriminant_analysis import StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import svm
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
+from sklearn.pipeline import make_pipeline
 
 randomState = 42
 
 # TODO: remove these declarations
 maxK = 7
 maxEstimators = 5
-maxC = 6
 maxClusters = 10
 
 def drawCorrelationMatrix(dataset):
@@ -120,21 +118,26 @@ for i in range(len(CM.data.columns)):
 
 print(f"\n> Drop highly correlated features, create synthetic ones and recompute correlation matrix")
 
-
 # Subtracting magnitudes gives the color
 dataset['u_g'] = dataset['u'] - dataset['g']
 dataset['g_r'] = dataset['g'] - dataset['r']
 dataset['r_i'] = dataset['r'] - dataset['i']
 dataset['i_z'] = dataset['i'] - dataset['z']
-datasetWithoutRedshift = dataset.drop(columns=['u', 'g', 'i', 'z', 'redshift'])
-print(f"\n\n> Optimized correlation matrix:\n{drawCorrelationMatrix(datasetWithoutRedshift).data}")
+dataset = dataset.drop(columns=['u', 'g', 'i', 'z', 'redshift'])
+for pair in pairs:
+    # Keep the first out of the pair of features
+    toDrop = pair[1]
+    if toDrop in dataset.columns:
+        dataset = dataset.drop(columns=[toDrop])
+        print(f"> Dropped feature: {toDrop}")
+print(f"\n\n> Optimized correlation matrix:\n{drawCorrelationMatrix(dataset).data}")
 
 
 # Split the dataset into features and target variable
 print(f"\n\n> Split dataset into features and target variable")
 targetName = "class"
-X = datasetWithoutRedshift.drop(columns=[targetName])
-y = datasetWithoutRedshift[targetName]
+X = dataset.drop(columns=[targetName])
+y = dataset[targetName]
 print(f"Features used for classification: {X.columns.to_list()}")
 print(f"Shape of features: {X.shape}")
 
@@ -144,13 +147,9 @@ print(f"\n\n> Split the dataset into Training and Testing sets (80%, 20%)")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=randomState)
 
 
-## Preprocessing 
-print(f"\n\n> Preprocessing\n> Given the logarithmic nature of the redshift feature, apply log1p transformation and scaling to it")
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
-X_train = pd.DataFrame(X_train_scaled, columns=X_train.columns)
-X_test = pd.DataFrame(X_test_scaled, columns=X_test.columns)
+## Preprocessing
+print(f"\n\n> Preprocessing (scaling)\n")
+
 
 
 
@@ -165,19 +164,19 @@ Ks = range(1, maxK)
 KNNcrossValidationScores = []
 print(f"\n\n> KNN training: evaluate KNearestNeighbors classifiers with K ranging from 1 to {max(Ks)} using 5-Fold Cross Validation and F1-score (macro) as metrics")
 for n_neighbors in Ks:
-    KNN = KNeighborsClassifier(n_neighbors=n_neighbors)
-    scores = cross_val_score(KNN, X_train, y_train, cv=5, scoring='f1_macro')
+    pipeline = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=n_neighbors))
+    scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='f1_macro')
     KNNcrossValidationScores.append(scores.mean())
     print(f"> n_neighbors = {n_neighbors}, F1-score (mean of batch) = {scores.mean():.4f}")
 
-# Evaluate which was the K that fit best
+# Extrapolate best K
 bestK = Ks[np.argmax(KNNcrossValidationScores)]
 
 # Retrain with that K to show graphs related to this iteration
 print(f"\n> Best k value for KNN found to be {bestK} with F1-score = {max(KNNcrossValidationScores):.4f}: Retraining KNN with best k to show related graphs")
-bestKNN = KNeighborsClassifier(bestK)
-bestKNN.fit(X_train, y_train)
-predVal = bestKNN.predict(X_test)
+bestKNNPipeline = make_pipeline(StandardScaler(), KNeighborsClassifier(bestK))
+bestKNNPipeline.fit(X_train, y_train)
+predVal = bestKNNPipeline.predict(X_test)
 
 plotQualityCheckGraph(KNNcrossValidationScores, bestK, max(KNNcrossValidationScores), "KNN")
 print(f"Accuracy: {accuracy_score(y_test, predVal):.4f}")
@@ -189,53 +188,92 @@ estimators = range(1, maxEstimators)
 RFcrossValidationScores = []
 print(f"\n\n> Random Forest Classifier training: evaluate Random Forest classifiers with n_estimators ranging from 1 to {max(estimators)} using 5-Fold Cross Validation and F1-score (macro) as metrics")
 for n_estimators in estimators:
-    RF = RandomForestClassifier(n_estimators=n_estimators, random_state=randomState)
-    scores = cross_val_score(RF, X_train, y_train, cv=5, scoring='f1_macro')
+    pipeline = make_pipeline(StandardScaler(), RandomForestClassifier(n_estimators=n_estimators, random_state=randomState))
+    scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='f1_macro')
     RFcrossValidationScores.append(scores.mean())
     print(f"> n_estimators = {n_estimators}, F1-score (mean of batch) = {scores.mean():.4f}")
 
+# Extrapolate best n_estimators
 bestEstimator = estimators[np.argmax(RFcrossValidationScores)]
 
 print(f"\n> Best validation F1-score: {max(RFcrossValidationScores):.4f} achieved at n_estimators = {bestEstimator}... Retrain Random Forest with best n_estimators to show related graphs")
-RF = RandomForestClassifier(n_estimators=bestEstimator, random_state=randomState)
-RF.fit(X_train, y_train)
-predVal = RF.predict(X_test)
+bestRFCPipeline = make_pipeline(StandardScaler(), RandomForestClassifier(n_estimators=bestEstimator, random_state=randomState))
+bestRFCPipeline.fit(X_train, y_train)
+predVal = bestRFCPipeline.predict(X_test)
 
 plotQualityCheckGraph(RFcrossValidationScores, bestEstimator, max(RFcrossValidationScores), "RFC")
 print(f"Accuracy: {accuracy_score(y_test, predVal):.4f}")
 
 
 
+# SVM training
+Cs = [0.001, 0.01, 0.1, 1, 10, 100]
+SVMcrossValidationScores = []
+print(f"\n\n> SVM training: evaluate SVM classifiers with C ranging from 1 to {max(Cs)} using 5-Fold Cross Validation and F1-score (macro) as metrics")
+for C in Cs:
+    LSVCPipeline = make_pipeline(StandardScaler(), LinearSVC(C=C, random_state=randomState))
+    scores = cross_val_score(LSVCPipeline, X_train, y_train, cv=5, scoring='f1_macro')
+    SVMcrossValidationScores.append(scores.mean())
+    print(f"> C = {C}, F1-score (mean of batch) = {scores.mean():.4f}")
+
+# Extrapolate best C
+bestC = Cs[np.argmax(SVMcrossValidationScores)]
+
+print(f"\n> Best validation F1-score: {max(SVMcrossValidationScores):.4f} achieved at C = {bestC}... Retrain SVM with best C to show related graphs")
+bestLSVCPipeline = make_pipeline(StandardScaler(), LinearSVC(C=bestC, random_state=randomState))
+bestLSVCPipeline.fit(X_train, y_train)
+predVal = bestLSVCPipeline.predict(X_test)
+plotQualityCheckGraph(SVMcrossValidationScores, bestC, max(SVMcrossValidationScores), "SVM")
+
+print(f"Accuracy: {accuracy_score(y_test, predVal):.4f}")
+
+
+
 ## Clustering
-print(f"\n\n> Clustering\n> Models to be implemented:\n1. KMeans\n2. GMM with PCA")
-# KMeans Clustering
-print(f"\n\n> KMeans Clustering")
-KMeansModel = KMeans(n_clusters=3, random_state=randomState)
-KMeanslabels = KMeansModel.fit_predict(X)
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=X.iloc[:, 0], y=X.iloc[:, 1], hue=KMeanslabels, palette='viridis', alpha=0.6)
-plt.title('KMeans Clustering on Original Data')
+print(f"\n\n> Clustering models to be implemented:\n1. KMeans\n2. GMM\nBoth models will be trained on \"raw\" data and on PCA-optimized data to compare results")
 
-
-## GMM optimized with PCA for dimensionality reduction
-print(f"\n\n> GMM optimized with PCA for dimensionality reduction")
+# Traing new scaler for clustering
+scaler = StandardScaler()
+X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 PCA = PCA(n_components=2, random_state=randomState)
-X_reduced = PCA.fit_transform(X)
-print(f"> Explained variance ratio by the 2 principal components: {PCA.explained_variance_ratio_}")
-loadings = pd.DataFrame(
-    PCA.components_.T, 
-    columns=['PC1', 'PC2'], 
-    index=X.columns
-)
-print("\n> Feature importance (PCA Loadings):\n", loadings)
+X_PCA = PCA.fit_transform(X_scaled)
 
-GMM_PCA = GaussianMixture(n_components=3, random_state=randomState)
-GMMlabels = GMM_PCA.fit_predict(X_reduced)
 
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=X_reduced[:, 0], y=X_reduced[:, 1], hue=GMMlabels, palette='viridis', alpha=0.6)
-plt.title('GMM Clustering on PCA reduced Data')
-plt.xlabel('Component1')
-plt.ylabel('Component2')
+# KMeans Clustering
+KmeansRAW = KMeans(n_clusters=3, random_state=randomState)
+raw_Kmeans = KmeansRAW.fit_predict(X_scaled)
+KmeansPCA = KMeans(n_clusters=3, random_state=randomState)
+PCA_Kmeans = KmeansPCA.fit_predict(X_PCA)
+
+
+# GMM Clustering
+GMMRAW = GaussianMixture(n_components=3, random_state=randomState)
+raw_GMM = GMMRAW.fit_predict(X_scaled)
+GMPPCA = GaussianMixture(n_components=3, random_state=randomState)
+PCA_GMM = GMPPCA.fit_predict(X_PCA)
+
+
+
+fig, axes = plt.subplots(2, 2, figsize=(18, 12))
+# KMeans Plots
+sns.scatterplot(x=X_PCA[:, 0], y=X_PCA[:, 1], hue=raw_Kmeans, palette='viridis', alpha=0.6, ax=axes[0, 0])
+axes[0, 0].set_title('KMeans (Trained on Raw Data)')
+axes[0, 0].set_xlabel('PC1')
+axes[0, 0].set_ylabel('PC2')
+sns.scatterplot(x=X_PCA[:, 0], y=X_PCA[:, 1], hue=PCA_Kmeans, palette='viridis', alpha=0.6, ax=axes[0, 1])
+axes[0, 1].set_title('KMeans (Trained on PCA Data)')
+axes[0, 1].set_xlabel('PC1')
+axes[0, 1].set_ylabel('PC2')
+
+# GMM Plots
+sns.scatterplot(x=X_PCA[:, 0], y=X_PCA[:, 1], hue=raw_GMM, palette='magma', alpha=0.6, ax=axes[1, 0])
+axes[1, 0].set_title('GMM (Trained on Raw Data)')
+axes[1, 0].set_xlabel('PC1')
+axes[1, 0].set_ylabel('PC2')
+sns.scatterplot(x=X_PCA[:, 0], y=X_PCA[:, 1], hue=PCA_GMM, palette='magma', alpha=0.6, ax=axes[1, 1])
+axes[1, 1].set_title('GMM (Trained on PCA Data)')
+axes[1, 1].set_xlabel('PC1')
+axes[1, 1].set_ylabel('PC2')
+
+plt.tight_layout()
 plt.show()
-